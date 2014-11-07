42b5c5647a3fb52d5ed106689e1b8d74639b535b
maat7043 <maatspencer@gmail.com>
2014-11-07 06:56:02 -0600

    Cancel and Register buttons Added

--- ARPG-IN-XNA/ARPG-IN-XNA/Screen Manager/Screens/Menus/registerForm.vb
+++ ARPG-IN-XNA/ARPG-IN-XNA/Screen Manager/Screens/Menus/registerForm.vb
@@ -1,12 +1,48 @@
-﻿Public Class registerForm
+﻿Imports System.IO
+Public Class registerForm
     Inherits BaseScreen
 
+    Private ovrCancel As Boolean = False
+    Private ovrRegister As Boolean = False
     Public Sub New()
         Name = "registerForm"
         State = ScreenState.Active
     End Sub
 
     Public Overrides Sub HandleInput()
+        ' Cancel Button
+        If Input.mMapX >= 360 And Input.mMapX <= 425 Then
+            If Input.mMapY >= 505 And Input.mMapY <= 525 Then
+                ovrCancel = True
+                If Input.Click = True Then
+                    ScreenManager.UnloadScreen("registerForm")
+                    TitleScreen.ovrForm = False
+                    ovrCancel = False
+                End If
+            Else
+                ovrCancel = False
+            End If
+        Else
+            ovrCancel = False
+        End If
+
+        ' Register Button
+        If Input.mMapX >= 460 And Input.mMapX <= 540 Then
+            If Input.mMapY >= 505 And Input.mMapY <= 525 Then
+                ovrRegister = True
+                If Input.Click = True Then
+                    ScreenManager.UnloadScreen("registerForm")
+                    TitleScreen.ovrForm = False
+                    ovrRegister = False
+                End If
+            Else
+                ovrRegister = False
+            End If
+        Else
+            ovrRegister = False
+        End If
+
+
 
     End Sub
     Public Overrides Sub Draw()
@@ -20,11 +56,20 @@
 
         ' Main Form
         Globals.SpriteBatch.Draw(Textures.registerForm, New Rectangle(X, Y, Width, Height), Color.White)
+
         ' Cancel
-        Globals.SpriteBatch.DrawString(Fonts.LargeROTMG, "Cancel", New Vector2(X + 0.4 * Width, Y + 0.9 * Height), Color.White, 0, New Vector2(0, 0), 0.9, SpriteEffects.None, 0)
+        If ovrCancel = False Then
+            Globals.SpriteBatch.DrawString(Fonts.LargeROTMG, "Cancel", New Vector2(X + 0.4 * Width, Y + 0.9 * Height), Color.White, 0, New Vector2(0, 0), 0.9, SpriteEffects.None, 0)
+        Else
+            Globals.SpriteBatch.DrawString(Fonts.LargeROTMG, "Cancel", New Vector2(X + 0.4 * Width, Y + 0.9 * Height), Color.LightGoldenrodYellow, 0, New Vector2(0, 0), 0.9, SpriteEffects.None, 0)
+        End If
 
         ' Register
-        Globals.SpriteBatch.DrawString(Fonts.LargeROTMG, "Register", New Vector2(X + 0.7 * Width, Y + 0.9 * Height), Color.White, 0, New Vector2(0, 0), 0.9, SpriteEffects.None, 0)
+        If ovrRegister = False Then
+            Globals.SpriteBatch.DrawString(Fonts.LargeROTMG, "Register", New Vector2(X + 0.7 * Width, Y + 0.9 * Height), Color.White, 0, New Vector2(0, 0), 0.9, SpriteEffects.None, 0)
+        Else
+            Globals.SpriteBatch.DrawString(Fonts.LargeROTMG, "Register", New Vector2(X + 0.7 * Width, Y + 0.9 * Height), Color.LightGoldenrodYellow, 0, New Vector2(0, 0), 0.9, SpriteEffects.None, 0)
+        End If
         Globals.SpriteBatch.End()
     End Sub
 
